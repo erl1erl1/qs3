0520f2b9d8e7e9708492edea556eaf7b0421798d
Your Name <erleronn@stud.ntnu.no>
2022-03-28 16:55:02 +0200

    Initial commit part 2

---
+++ backend/src/main/java/idi/bidata/burritobanden/qs3/Qs3Application.java
@@ -0,0 +1,13 @@
+package idi.bidata.burritobanden.qs3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Qs3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Qs3Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/idi/bidata/burritobanden/qs3/Qs3ApplicationTests.java
@@ -0,0 +1,13 @@
+package idi.bidata.burritobanden.qs3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Qs3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
