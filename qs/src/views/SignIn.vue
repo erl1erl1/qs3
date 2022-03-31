87ac56b1f773a791b10d6731c87987be744dbf4a
Brilleslangen <nicolaiSivesind@gmail.com>
2022-04-01 00:02:38 +0200

    Sign-in and Sign-up is now complete😍

--- qs/src/views/SignIn.vue
+++ qs/src/views/SignIn.vue
@@ -1,14 +1,12 @@
 <template>
-  <img alt="logo" id="logo" src="@/assets/qs-dark.svg">
-  <div id="form-container">
+  <img alt="logo" class="logo" src="@/assets/qs-dark.svg">
+  <div class="form-container">
     <h1>Pålogging</h1>
     <Form @submit="onSubmit" v-slot="{ meta }">
       <div class="input-container">
         <label>Brukernavn</label>
         <Field class="input" rules="required|alpha_num" name="username" type="text" placeholder="brukernavn" validateOnInput/>
-        <ErrorMessage class="error" name="username"/>
       </div>
-
       <div class="input-container">
         <label>Passord</label>
         <Field class="input" rules="required" name="password" type="password" placeholder="passord" validateOnInput/>
@@ -16,12 +14,12 @@
       <button class="button" :disabled="!(meta.valid)">Logg inn</button>
     </Form>
     <p v-if="!this.correctPassword" style="color: red">Incorrect credentials</p>
-    <p style="margin-bottom: 10px">Ikke registrert? <router-link as="a" class="link" to="SignUp">Lag en bruker</router-link></p>
+    <p style="margin-bottom: 10px">Har du ikke en bruker? <router-link as="a" class="link" to="SignUp">Registrer deg</router-link></p>
   </div>
 </template>
 
 <script>
-import { Form, Field, ErrorMessage } from 'vee-validate'
+import { Form, Field} from 'vee-validate'
 import { mapActions } from 'vuex'
 
 export default {
@@ -36,7 +34,6 @@ export default {
   components: {
     Form,
     Field,
-    ErrorMessage,
   },
 
   methods: {
@@ -67,17 +64,3 @@ export default {
   }
 }
 </script>
-
-<style scoped>
-#form-container {
-  min-height: 300px;
-  border-radius: 10px;
-  text-align: center;
-}
-
-#logo {
-  width: 60%;
-  max-width: 400px;
-  margin: 2em 0 2em 0 ;
-}
-</style>
