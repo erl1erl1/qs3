8b5abfab94c9549c6b88f430ade9e62706379515
Brilleslangen <nicolaiSivesind@gmail.com>
2022-04-03 16:36:40 +0200

    converted textfields to components to make html less messy

--- qs/src/views/Register.vue
+++ qs/src/views/Register.vue
@@ -3,34 +3,15 @@
   <div class="form-container">
     <h1>Registrer bruker</h1>
     <Form @submit="onSubmit" v-slot="{ meta }">
-      <div class="input-container">
-        <label>Fullt navn</label>
-        <Field rules="required|alpha_spaces" name="name" type="text" placeholder="Fullt navn" validateOnBlur/>
-        <span class="error">
-          <ErrorMessage name="name">Navn kan kun inneholde bokstaver</ErrorMessage>
-        </span>
-      </div>
-      <div class="input-container">
-        <label>Brukernavn</label>
-        <Field rules="required|alpha_num|min_max:3,16" name="username" type="text" placeholder="brukernavn" validateOnBlur/>
-        <span class="error">
-          <ErrorMessage name="username">Brukernavn må være mellom 3 og 16 karakterer langt</ErrorMessage>
-        </span>
-      </div>
-      <div class="input-container">
-        <label>Passord</label>
-        <Field rules="required" name="password" type="password" placeholder="passord" validateOnBlur/>
-        <span class="error">
-          <ErrorMessage name="password">Passord kan ikke være tomt</ErrorMessage>
-        </span>
-      </div>
-      <div class="input-container">
-        <label>Bekreft passord</label>
-        <Field rules="required|confirmed:@password"  name="confirm" type="password" placeholder="passord" validateOnInput/>
-        <span class="error">
-         <ErrorMessage name="confirm">Passordene samstemmer ikke</ErrorMessage>
-        </span>
-      </div>
+      <TextInput label="Fullt navn" name="name" placeholder="Fullt navn" type="text"
+                 rules="required|alpha_spaces" error-message="Navn kan kun inneholde bokstaver"/>
+      <TextInput label="Brukernavn" name="username" placeholder="brukernavn" type="text"
+                 rules="required|alpha_num|min_max:3,16" error-message="Brukernavn må være mellom 3 og 16 karakterer langt"/>
+      <TextInput label="Passord" name="password" placeholder="passord" type="password"
+                 rules="required" error-message="Passord kan ikke være tomt"/>
+      <TextInput label="Bekreft passord" name="confirm" placeholder="passord" type="password"
+                 rules="required|confirmed:@password" error-message="Passordene samstemmer ikke" validate-on-input
+      />
 
       <button :disabled="!(meta.valid)">Registrer deg</button>
     </Form>
@@ -39,21 +20,26 @@
 </template>
 
 <script>
-import { Form, Field, ErrorMessage } from 'vee-validate'
+import { Form } from 'vee-validate'
+import TextInput from "@/components/form/TextInput";
 
 export default {
   name: "Login",
 
   data() {
     return {
-
+      initialErrors: {
+        name: "Navn kan kun inneholde bokstaver",
+        username: "Brukernavn må være mellom 3 og 16 karakterer langt",
+        password: "Passord kan ikke være tomt",
+        confirm: "Passordene samstemmer ikke",
+      }
     }
   },
 
   components: {
+    TextInput,
     Form,
-    Field,
-    ErrorMessage,
   },
 
   methods: {
